a2f5b7d496b602bba9dd7e0abb2afc94e87e5692
jm server2 <jm@mazoea.com>
2022-11-16 09:33:39 +0100

    [devOps] moving to github

--- build-scripts/build.docker.ubuntu.bat
+++ build-scripts/build.docker.ubuntu.bat
@@ -1,4 +1,4 @@
 cd ..
-docker pull registry.gitlab.com/mazoea-team/docker-i2t-bits
-docker run --rm -it -e TE_LIBS=/opt/mazoea/installation -e GCCVERSION=8 -v %cd%:/opt/src registry.gitlab.com/mazoea-team/docker-i2t-bits:latest /bin/bash -c "cd scripts && ./os.specific.sh && ./compile.sh"
+docker pull ghcr.io/mazoea/docker-i2t-bits
+docker run --rm -it -e TE_LIBS=/opt/mazoea/installation -e GCCVERSION=8 -v %cd%:/opt/src ghcr.io/mazoea/docker-i2t-bits:latest /bin/bash -c "cd scripts && ./os.specific.sh && ./compile.sh"
 pause
--- build-scripts/run.docker.ubuntu.bat
+++ build-scripts/run.docker.ubuntu.bat
@@ -1,5 +1,5 @@
 REM test for GLIBC ` strings libleptonica1.so.1.78.0 | grep ^GLIBC_`
 cd ..
-docker pull registry.gitlab.com/mazoea-team/docker-i2t-bits:v8
-docker run --rm -it -v %cd%:/opt/src registry.gitlab.com/mazoea-team/docker-i2t-bits:latest /bin/bash
+docker pull ghcr.io/mazoea/docker-i2t-bits:latest
+docker run --rm -it -v %cd%:/opt/src ghcr.io/mazoea/docker-i2t-bits:latest /bin/bash
 pause
